6142c0691230a02b4192b497d223ce45864d6865
Andreas Knöpfle <andi@bitcrowd.net>
2025-04-07 18:26:50 +0200

    [#EX-40] Abstract of TOS for consent page: (#42)
    - **Welcome to Exmeralda:** Exmeralda is your AI-powered assistant,
      provided with love by bitcrowd, designed to help you with interactive
      documentation for various open source software packages.&#x20;
    - **Your Privacy Matters:** To protect privacy, please don't input
      personal data into Exmeralda. We do use your interactions to improve
      our service and help package creators, but rest assured, data is
      anonymized. You can always opt out or request to delete your account
      at any time.
    - **Using Exmeralda Responsibly:** You’re welcome to use Exmeralda’s
      content for internal business purposes. While we strive for accuracy,
      keep in mind the occasional errors or third-party content we can't
      fully control. We'll always do our best to provide reliable service,
      but can't guarantee perfection. Please be aware that the open source
      software packages might have licence restrictions.
    - **Keeping You in the Loop:** If our terms change significantly, we'll
      give you advance notice. And although it's rare, we might need to
      suspend an account if it violates our guidelines. If any issues arise,
      German law applies, with Berlin as the agreed place of jurisdiction.
    
    https://bitcrowd.atlassian.net/browse/EX-40

--- lib/exmeralda_web/live/user_live/accept_terms.ex
+++ lib/exmeralda_web/live/user_live/accept_terms.ex
@@ -49,7 +49,7 @@ defmodule ExmeraldaWeb.UserLive.AcceptTerms do
     ~H"""
     <.navbar_layout user={@current_user}>
       <.hero_layout inside_navbar>
-        <div class="text-center flex flex-col gap-8">
+        <div class="items-center flex flex-col gap-8">
           <h1 class="text-5xl font-bold">Terms and Conditions</h1>
           <img
             src={~p"/images/logo-exmeralda.svg"}
@@ -58,20 +58,48 @@ defmodule ExmeraldaWeb.UserLive.AcceptTerms do
             alt="Exmeralda logo, with stylised circuit board tracks surrounding a central node"
             class="max-w-xs py-5"
           />
-          <div class="card bg-base-100 w-full shrink-0 shadow-2xl">
-            <div class="card-body">
+          <div class="card bg-base-100 shrink-0 shadow-2xl w-full md:w-2/3">
+            <div class="card-body text-left">
               <div role="alert" class="alert alert-info">
                 <.icon name="hero-information-circle" />
                 <span>Please accept the terms of service to continue.</span>
               </div>
-              <iframe class="w-full h-96" src="/terms" title="Terms of service"></iframe>
+              <ul class="flex flex-col gap-6 list-disc mx-6 my-4">
+                <li>
+                  <strong>Welcome to Exmeralda:</strong>
+                  Exmeralda is your AI-powered assistant, provided  with love by bitcrowd,
+                  designed to help you with interactive documentation for various open source software packages.
+                </li>
+                <li>
+                  <strong>Your Privacy Matters:</strong>
+                  To protect privacy, please don't input personal data into Exmeralda.
+                  We do use your interactions to improve our service and help package creators, but rest assured,
+                  data is anonymized. You can always opt out or request to delete your account at any time.
+                </li>
+                <li>
+                  <strong>Using Exmeralda Responsibly:</strong>
+                  You're welcome to use Exmeralda's content for internal business purposes. While we strive for accuracy,
+                  keep in mind the occasional errors or third-party content we can't fully control. We'll always do our best
+                  to provide reliable service, but can't guarantee perfection. Please be aware that the open source software
+                  packages might have licence restrictions.
+                </li>
+                <li>
+                  <strong>Keeping You in the Loop:</strong>
+                  If our terms change significantly, we'll give you advance notice. And although it's rare, we might need
+                  to suspend an account if it violates our guidelines. If any issues arise, German law applies, with Berlin
+                  as the agreed place of jurisdiction.
+                </li>
+              </ul>
               <.simple_form for={@form} phx-submit="accept">
                 <.input
                   type="checkbox"
                   field={@form[:terms_accepted]}
-                  label="I agree to the Terms and Conditions"
+                  label={
+                    raw(
+                      "<span>I agree to the <a href=\"/terms\" target=\"_blank\" class=\"link\">Terms and Conditions</a></span>"
+                    )
+                  }
                 />
-                <p></p>
                 <:actions>
                   <.button class="btn-primary">Continue</.button>
                 </:actions>
