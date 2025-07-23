47d7442eebb13bfadb3239023b4b4d8d7891e4a1
Joel Koch <joel@bitcrowd.net>
2025-07-23 15:31:41 +0200

    Add ingestion_id to chat session

--- lib/exmeralda/chats/session.ex
+++ lib/exmeralda/chats/session.ex
@@ -1,7 +1,7 @@
 defmodule Exmeralda.Chats.Session do
   use Exmeralda.Schema
   alias Exmeralda.Accounts.User
-  alias Exmeralda.Topics.Library
+  alias Exmeralda.Topics.{Ingestion, Library}
   alias Exmeralda.Chats.Message
 
   @title_max_length 255
@@ -11,6 +11,7 @@ defmodule Exmeralda.Chats.Session do
     field :prompt, :string, virtual: true
     belongs_to :user, User
     belongs_to :library, Library
+    belongs_to :ingestion, Ingestion
     has_many :messages, Message, preload_order: [asc: :index]
 
     timestamps()
@@ -20,8 +21,8 @@ defmodule Exmeralda.Chats.Session do
   def create_changeset(session, attrs) do
     changeset =
       session
-      |> cast(attrs, [:library_id, :prompt])
-      |> validate_required([:library_id, :prompt])
+      |> cast(attrs, [:library_id, :ingestion_id, :prompt])
+      |> validate_required([:library_id, :ingestion_id, :prompt])
 
     title =
       changeset
