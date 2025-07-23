1fdea91dfb7d2133356f26d5aa07f8364eb86156
Joel Koch <joel@bitcrowd.net>
2025-07-23 14:55:04 +0200

    search_libraries/1 returns libraries with ready ingestions

--- test/exmeralda/topics_test.exs
+++ test/exmeralda/topics_test.exs
@@ -2,7 +2,7 @@ defmodule Exmeralda.TopicsTest do
   use Exmeralda.DataCase
 
   alias Exmeralda.Topics
-  alias Exmeralda.Topics.Chunk
+  alias Exmeralda.Topics.Ingestion
 
   def insert_ingested_library(_) do
     library = insert(:library, name: "ecto")
@@ -16,7 +16,7 @@ defmodule Exmeralda.TopicsTest do
     ingestion = insert(:ingestion, library: library, state: :embedding)
     insert_list(3, :chunk, ingestion: ingestion, library: library)
     insert_list(1, :chunk, ingestion: ingestion, library: library, embedding: nil)
-    %{in_progress: library}
+    %{in_progress: library, in_progress_ingestion: ingestion}
   end
 
   def insert_chunkless_library(_) do
@@ -46,14 +46,15 @@ defmodule Exmeralda.TopicsTest do
     test "returns only libraries that are ingested fully and match the term", %{
       ingested: ingested,
       chunkless: chunkless,
-      in_progress: in_progress
+      in_progress: in_progress,
+      in_progress_ingestion: in_progress_ingestion
     } do
       ids = Topics.search_libraries("ecto") |> Enum.map(& &1.id)
       assert ingested.id in ids
-      assert chunkless.id in ids
+      refute chunkless.id in ids
       refute in_progress.id in ids
 
-      from(c in Chunk, where: is_nil(c.embedding)) |> Exmeralda.Repo.delete_all()
+      Ingestion.set_state(in_progress_ingestion, :ready) |> Repo.update!()
 
       ids = Topics.search_libraries("ecto") |> Enum.map(& &1.id)
       assert in_progress.id in ids
