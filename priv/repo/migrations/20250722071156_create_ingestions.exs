5b6205a8ef38de139c2bd5a36bdad26d76a48bac
Joel Koch <joel@bitcrowd.net>
2025-07-24 09:18:06 +0200

    Create ingestion for each library

--- priv/repo/migrations/20250722071156_create_ingestions.exs
+++ priv/repo/migrations/20250722071156_create_ingestions.exs
@@ -1,6 +1,8 @@
 defmodule Exmeralda.Repo.Migrations.CreateIngestions do
   use Ecto.Migration
 
+  import Ecto.Query
+
   def up do
     execute(
       "CREATE TYPE ingestion_state AS ENUM ('queued', 'preprocessing', 'chunking', 'embedding', 'failed', 'ready');"
@@ -12,6 +14,31 @@ defmodule Exmeralda.Repo.Migrations.CreateIngestions do
 
       timestamps()
     end
+
+    flush()
+
+    libraries_ids =
+      repo().all(
+        from(l in "libraries",
+          left_join: i in "ingestions",
+          on: i.library_id == l.id,
+          where: is_nil(i.library_id),
+          select: l.id
+        )
+      )
+
+    ingestions =
+      Enum.map(libraries_ids, fn library_id ->
+        %{
+          id: Ecto.UUID.bingenerate(),
+          state: "ready",
+          library_id: library_id,
+          inserted_at: DateTime.utc_now(),
+          updated_at: DateTime.utc_now()
+        }
+      end)
+
+    repo().insert_all("ingestions", ingestions)
   end
 
   def down do
