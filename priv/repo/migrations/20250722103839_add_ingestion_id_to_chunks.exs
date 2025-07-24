7331c314c17c648fc6fc6aa62e061bafbdd53325
Joel Koch <joel@bitcrowd.net>
2025-07-24 09:19:12 +0200

    Associate each chunk with ingestion for its library

--- priv/repo/migrations/20250722103839_add_ingestion_id_to_chunks.exs
+++ priv/repo/migrations/20250722103839_add_ingestion_id_to_chunks.exs
@@ -5,6 +5,29 @@ defmodule Exmeralda.Repo.Migrations.AddIngestionIdToChunks do
     alter table(:chunks) do
       add :ingestion_id, references(:ingestions, on_delete: :delete_all), null: true
     end
+
+    flush()
+
+    execute("""
+      UPDATE
+        chunks
+      SET
+        ingestion_id = ingestions.id
+      FROM
+        ingestions
+      WHERE
+        chunks.ingestion_id IS NULL
+        AND chunks.library_id = ingestions.library_id
+    """)
+
+    flush()
+
+    execute("""
+      ALTER TABLE
+        chunks ALTER COLUMN ingestion_id
+      SET
+        NOT NULL;
+    """)
   end
 
   def down do
