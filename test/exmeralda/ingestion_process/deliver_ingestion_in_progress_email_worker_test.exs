9778f5c05ec94c9a2af2ad367db63314f962621a
Joel Koch <joel@bitcrowd.net>
2025-08-07 10:14:35 +0200

    Include library name in email

--- test/exmeralda/ingestion_process/deliver_ingestion_in_progress_email_worker_test.exs
+++ test/exmeralda/ingestion_process/deliver_ingestion_in_progress_email_worker_test.exs
@@ -11,13 +11,19 @@ defmodule Exmeralda.IngestionProcess.DeliverIngestionInProgressEmailWorkerTest d
 
   describe "perform/1" do
     test "delivers email to user", %{user: user} do
+      library_name = "rag"
+
       assert {:ok, _} =
                perform_job(DeliverIngestionInProgressEmailWorker, %{
                  name: user.name,
-                 email: user.email
+                 email: user.email,
+                 library_name: library_name
                })
 
-      assert_email_sent(subject: "We are ingesting your library", to: {user.name, user.email})
+      assert_email_sent(
+        subject: "We are ingesting the package #{library_name} for you",
+        to: {user.name, user.email}
+      )
     end
   end
 end
