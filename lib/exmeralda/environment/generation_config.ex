8cf101554ee430dcf1d29d1e4bab5b41f731ddb9
Agathe Lenclen <agathe@bitcrowd.net>
2025-08-25 16:53:55 +0200

    Add docs

---
+++ lib/exmeralda/environment/generation_config.ex
@@ -0,0 +1,14 @@
+defmodule Exmeralda.Environment.GenerationConfig do
+  @moduledoc """
+  GenerationConfig stores the various parameters that can be associated
+  to generating a message from an agent.
+
+  """
+  use Exmeralda.Schema
+  alias Exmeralda.Environment.{ModelConfigProvider}
+
+  schema "generation_configs" do
+    belongs_to(:model_config_provider, ModelConfigProvider)
+    # System prompt and template prompt will be added later.
+  end
+end
---
+++ priv/repo/migrations/20250825142625_create_generation_config.exs
@@ -0,0 +1,14 @@
+defmodule Exmeralda.Repo.Migrations.CreateGenerationConfig do
+  use Ecto.Migration
+
+  def change do
+    create table(:generation_configs) do
+      add :model_config_provider_id, references(:model_config_providers, on_delete: :restrict),
+        null: false
+
+      timestamps()
+    end
+
+    create unique_index(:generation_configs, [:model_config_provider_id])
+  end
+end
