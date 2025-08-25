bdf71977ea49d2077a1d392b04110b2ecb86b437
Agathe Lenclen <agathe@bitcrowd.net>
2025-08-25 16:55:19 +0200

    [EX-98] Setup a default Provider and ModelConfig as runtime variables

---
+++ priv/repo/migrations/20250825153843_add_generation_config_to_message.exs
@@ -0,0 +1,9 @@
+defmodule Exmeralda.Repo.Migrations.AddGenerationConfigToMessage do
+  use Ecto.Migration
+
+  def change do
+    alter table(:chat_messages) do
+      add :generation_config_id, references(:generation_configs, on_delete: :restrict),
+    end
+  end
+end
