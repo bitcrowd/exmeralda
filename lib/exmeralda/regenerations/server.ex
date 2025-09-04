f4f93d715cdfe1aa42139a991d9b35868b16260a
Agathe Lenclen <agathe@bitcrowd.net>
2025-09-04 14:38:29 +0200

    Add tests

--- lib/exmeralda/regenerations/server.ex
+++ lib/exmeralda/regenerations/server.ex
@@ -2,46 +2,67 @@ defmodule Exmeralda.Regenerations.Server do
   use GenServer
   require Logger
   alias Exmeralda.Regenerations
-  alias Exmeralda.Repo
-  alias Ecto.Multi
 
   def init(%{message_ids: message_ids, generation_environment_id: generation_environment_id}) do
     Phoenix.PubSub.subscribe(Exmeralda.PubSub, "regenerations")
 
     {:ok,
      %{
-       completion: Map.new(message_ids, fn message_id -> {message_id, false} end),
        message_ids: message_ids,
-       generation_environment_id: generation_environment_id
+       generation_environment_id: generation_environment_id,
+       regenerated_messages: %{},
+       skipped_messages: %{}
      }}
   end
 
   def handle_call(:regenerate, _from, state) do
-    Enum.reduce(state.message_ids, Multi.new(), fn message_id, multi ->
-      Multi.run(
-        multi,
-        {:message, message_id},
-        fn _, _ -> Regenerations.regenerate(message_id, state.generation_environment_id) end
-      )
-    end)
-    |> Repo.transaction()
-    |> case do
-      {:ok, result} -> {:reply, result, state}
-      {:error, step, error, _} -> {:stop, {step, error}, state}
-    end
+    state =
+      Enum.reduce(state.message_ids, state, fn message_id, acc ->
+        case Regenerations.regenerate(message_id, state.generation_environment_id) do
+          {:ok, %{assistant_message: assistant_message}} ->
+            regenerated_messages =
+              Map.put(acc.regenerated_messages, message_id, %{
+                assistant_message_id: assistant_message.id,
+                incomplete: true
+              })
+
+            %{acc | regenerated_messages: regenerated_messages}
+
+          {:error, error} ->
+            skipped_messages = Map.put(acc.skipped_messages, message_id, error)
+            %{acc | skipped_messages: skipped_messages}
+        end
+      end)
+
+    {:reply, state, state}
   end
 
   def handle_info({:message_regenerated, message_id}, state) do
-    completion = Map.replace!(state.completion, message_id, true)
-    completion_status = Map.values(completion)
+    case Map.get(state.regenerated_messages, message_id) do
+      nil ->
+        {:noreply, state}
 
-    Logger.info("⌛️ Message #{message_id} regenerated!")
+      %{assistant_message_id: assistant_message_id} ->
+        Logger.info("⌛️ Message #{message_id} regenerated!")
 
-    if Enum.all?(completion_status) do
-      Phoenix.PubSub.unsubscribe(Exmeralda.PubSub, "regenerations")
-      {:stop, :normal, %{state | completion: completion}}
-    else
-      {:noreply, %{state | completion: completion}}
+        regenerated_messages =
+          Map.replace!(state.regenerated_messages, message_id, %{
+            assistant_message_id: assistant_message_id,
+            incomplete: false
+          })
+
+        if all_regenerated_messages_complete?(regenerated_messages) do
+          Phoenix.PubSub.unsubscribe(Exmeralda.PubSub, "regenerations")
+          {:stop, :normal, %{state | regenerated_messages: regenerated_messages}}
+        else
+          {:noreply, %{state | regenerated_messages: regenerated_messages}}
+        end
     end
+  end
+
+  defp all_regenerated_messages_complete?(regenerated_messages) do
+    regenerated_messages
+    |> Map.values()
+    |> Enum.all?(&(!&1.incomplete))
   end
 end
