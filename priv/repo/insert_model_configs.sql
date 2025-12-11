9429133c5d87d501c5b55ab5fc07337149b478cb
Max Mulatz <max@bitcrowd.net>
2025-12-11 22:11:18 +0100

    Remove migration to change production model
    This removes the migration to update the production model. The migration
    only includes INSERT statements and a conditional to check for the
    current environment. Both are anti-patterns and will make our lives
    harder…
    
    So I opted for some raw SQL juggle instead and added the new model to
    the large SQL statement, more as a way to document it.

--- priv/repo/insert_model_configs.sql
+++ priv/repo/insert_model_configs.sql
@@ -9,6 +9,7 @@ INSERT INTO model_configs (id, name, config, inserted_at, updated_at)
     VALUES
       (gen_random_uuid(), 'qwen25-coder-32b', '{"stream": true}', NOW(), NOW()),
       (gen_random_uuid(), 'llama-4-maverick-17b-128e', '{"stream": true}', NOW(), NOW());
+      (gen_random_uuid(), 'qwen25-7b-instruct-turbo', '{"stream": true}', NOW(), NOW());
 
 INSERT INTO model_config_providers (id, model_config_id, provider_id, name, inserted_at, updated_at)
     VALUES
@@ -49,6 +50,14 @@ INSERT INTO model_config_providers (id, model_config_id, provider_id, name, inse
         (SELECT id FROM model_configs WHERE name='llama-4-maverick-17b-128e'),
         (SELECT id FROM providers WHERE name='groq_ai'),
         'meta-llama/llama-4-maverick-17b-128e-instruct',
+        NOW(),
+        NOW()
+      ),
+      (
+        gen_random_uuid(),
+        (SELECT id FROM model_configs WHERE name='qwen25-7b-instruct-turbo'),
+        (SELECT id FROM providers WHERE name='together_ai'),
+        'Qwen/Qwen2.5-7B-Instruct-Turbo',
         NOW(),
         NOW()
       );
